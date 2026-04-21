ae259491f0c6f721b27422660de4721f18c1ba85
Rudro Roy <royrudro032@gmail.com>
2026-04-21 14:56:09 +0600

    feat: quantityselector component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,26 @@
 import React from "react";
 import "./index.css";
-import CardLayout from "./components/layout/Navbar.jsx";
+import CardModal from "./components/layout/CartModal.jsx";
 import Navbar from "./components/layout/Navbar.jsx";
 import ProductGallery from "./components/product/ProductGallery.jsx";
 import QuantitySelector from "./components/product/QuantitySelector.jsx";
 import Thumbnail from "./components/product/Thumbnail.jsx";
-import Badge from "./components/ui/Badge.jsx";
-import Button from "./components/ui/Button.jsx";
-const handleAddToCart = () => {
-  // add to cart logic here
-  console.log("Product added to cart!");
-};
 
 const App = () => {
+  const [quantity, setQuantity] = React.useState(0);
+  const handleIncrease = () => {
+    setQuantity((prev) => prev + 1);
+  };
+  const handleDecrease = () => {
+    setQuantity((prev) => (prev > 0 ? prev - 1 : 0));
+  };
   return (
     <main>
       <Navbar />
+      <CardModal />
       <ProductGallery />
-      <QuantitySelector />
+      <QuantitySelector quantity={quantity} increase={handleIncrease} decrease={handleDecrease} />
       <Thumbnail />
-      <div className="relative w-fit cursor-pointer">
-        <img src="/images/icon-cart.svg" alt="Cart" />
-        <Badge
-          variant="cart"
-          className="absolute -top-2 -right-2 border-2 border-white"
-        >
-          3
-        </Badge>
-      </div>
-      <Badge variant="discount">50%</Badge>
-      <Button onClick={handleAddToCart}>
-        <img src="/images/icon-cart.svg" alt="" aria-hidden="true" />
-        <span>Add to cart</span>
-      </Button>
     </main>
   );
 };
--- src/components/product/QuantitySelector.jsx
+++ src/components/product/QuantitySelector.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const QuantitySelector = () => {
+const QuantitySelector = ({ quantity, increase, decrease }) => {
   return (
-    <div>QuantitySelector</div>
-  )
-}
+    <div className="flex items-center justify-between bg-light-grayish-blue rounded-lg py-3 px-4 w-full md:w-40">
+      <button onClick={decrease} aria-label="Decrease quantity" className="cursor-pointer py-2">
+        <img
+          src="./images/icon-minus.svg"
+          className="hover:opacity-60 transition-opacity"
+          alt=""
+        />
+      </button>
+      <span className="font-bold text-very-dark-blue tabular-nums">{quantity}</span>
+      <button onClick={increase} aria-label="Increase quantity">
+        <img
+          src="./images/icon-plus.svg"
+          className="hover:opacity-60 transition-opacity"
+          alt=""
+        />
+      </button>
+    </div>
+  );
+};
 
-export default QuantitySelector
+export default QuantitySelector;
